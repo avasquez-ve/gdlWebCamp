7c35817f29255f784fbd9d64002a5f4e673c9fed
Arnell Vasquez <arnellvc@gmail.com>
2019-04-29 23:51:33 -0400

    Agregada animaciones para resto de numeros

--- js/main.js
+++ js/main.js
@@ -164,5 +164,8 @@ $(function () {
     })
     //Animaciones para números
     $(".resumen-evento li:nth-child(1) p").animateNumber({number: 6}, 1200);
+    $(".resumen-evento li:nth-child(2) p").animateNumber({number: 15}, 1200);
+    $(".resumen-evento li:nth-child(3) p").animateNumber({number: 3}, 2000);
+    $(".resumen-evento li:nth-child(4) p").animateNumber({number: 9}, 1500);
 
 });
